8d017594bb08952af0504413687ed34387478cc7
Nikhil-C82 <nikhil88c@gmail.com>
2022-08-11 20:45:44 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+const db = require('./config/mongoose');
+
+// used for cookies session
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+
+//MongoStore
+const MongoStore = require('connect-mongo')(session);
+const sassMiddleware = require('node-sass-middleware');
+const flash = require('connect-flash');
+const customMware = require('./config/middleware');
+
+app.use(sassMiddleware({
+    src:'./assets/scss',
+    dest:'./assets/css',
+    debug:true,
+    outputStyle:'extended',
+    prefix: '/css'
+}));
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cookieParser());
+
+app.use(express.static('./assets'));
+
+
+app.use(expressLayouts);
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
+app.set('view engine','ejs');
+app.set('views','./views');
+
+//session
+app.use(session({
+    name:'codial',
+    secret:'blahsomething',
+    saveUninitialized:false,
+    resave:false,
+    cookie:{
+        maxAge:(1000 * 60 * 100)
+    },
+    store:new MongoStore(
+        {
+            mongooseConnection: db,
+            autoRemove: 'disabled'
+        
+        },
+        function(err){
+            console.log(err ||  'connect-mongodb setup ok');
+        }
+    )
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+app.use(flash());
+app.use(customMware.setFlash);
+
+app.use('/',require('./routers'));
+
+
+
+app.listen(port,function(err){
+    if(err){
+        console.log(`Error in running the server: ${err}`);
+    }
+
+    console.log(`Server Running On Port : ${port}`);
+});
+
